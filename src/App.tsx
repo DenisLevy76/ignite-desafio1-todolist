f3dbf7ac87eceb22bb20c135e9e69645110f4594
Denis Levy de M. Cravo <denis.levi11@gmail.com>
2022-12-24 15:25:19 -0300

    Add empty task list style

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import { PlusCircle } from 'phosphor-react';
+import { ClipboardText } from 'phosphor-react';
 import { HeaderComponent } from './components/HeaderComponent';
 
 import styles from './styles/app.module.css';
 import './styles/reset.css';
+
+const tasks: string[] = [];
 
 export const App = () => {
   return (
@@ -20,6 +23,29 @@ export const App = () => {
             Criar <PlusCircle size={24} />
           </button>
         </form>
+
+        <section className={styles.tasks}>
+          <header>
+            <h4 className={styles['number-of-tasks']}>
+              Tarefas criadas <span>0</span>
+            </h4>
+            <h4 className={styles['done-tasks']}>
+              Concluídas <span>0</span>
+            </h4>
+          </header>
+
+          <ul className={styles['tasks__list']}>
+            {tasks.length > 0 ? (
+              tasks.map((task) => <li>{task}</li>)
+            ) : (
+              <div className={styles.tasks__empty}>
+                <ClipboardText size={64} weight="thin" />
+                <strong>Você ainda não tem tarefas cadastradas</strong>
+                <p>Crie tarefas e organize seus itens a fazer</p>
+              </div>
+            )}
+          </ul>
+        </section>
       </main>
     </div>
   );
