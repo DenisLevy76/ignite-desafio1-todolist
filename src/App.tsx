e4bd4bce173d303ae02009d2760bb7a46e3da9be
Denis Levy de M. Cravo <denis.levi11@gmail.com>
2022-12-24 14:57:25 -0300

    Add create task form styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
+import { PlusCircle } from 'phosphor-react';
 import { HeaderComponent } from './components/HeaderComponent';
 
+import styles from './styles/app.module.css';
 import './styles/reset.css';
 
 export const App = () => {
   return (
-    <>
+    <div>
       <HeaderComponent />
-    </>
+      <main className={styles.container}>
+        <form className={styles['create-task-form']}>
+          <input
+            type="text"
+            name="taskName"
+            id="taskName"
+            placeholder="Adicione uma nova tarefa"
+          />
+          <button type="submit">
+            Criar <PlusCircle size={24} />
+          </button>
+        </form>
+      </main>
+    </div>
   );
 };
